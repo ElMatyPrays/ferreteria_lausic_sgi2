3b8923708456d2eaca54555d9fb2fb472cdca9c3
Ezpiboll <ariel.medel03@inacapmail.cl>
2026-01-15 19:02:51 -0300

    casi casi

--- backend/src/database/dbORM.ts
+++ backend/src/database/dbORM.ts
@@ -1,6 +1,9 @@
 // src/database/dbORM.ts
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // entidades
 import { Registro_ventaEntity } from "./entities/registro_ventaEntity";
@@ -10,12 +13,14 @@ import { UsuarioEntity } from "./entities/usuarioEntity";
 import { ClienteEntity } from "./entities/clienteEntity";
 
 export const AppDataSource = new DataSource({
+
+  
   type: "mysql",
-  host: "127.0.0.1",
-  port: 3306,
-  username: "root",
-  password: "",
-  database: "ferreteria_lausic_db",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
   synchronize: false,
   logging: false,
